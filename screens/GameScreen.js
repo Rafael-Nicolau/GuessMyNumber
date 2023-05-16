db62db937ec19acac24827ff0c07d74a29128d68
Rafael Nicolau <rafael.nicolau@sedo.com>
2023-05-16 23:36:44 +0200

    Initial Pre-working Alpha

---
+++ screens/GameScreen.js
@@ -0,0 +1,101 @@
+import { View, Text, StyleSheet, Alert } from "react-native";
+import { useEffect, useState } from "react";
+import Title from "../components/ui/Title";
+import Colors from "../constants/colors";
+import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
+
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+let minBoundary = 1;
+let maxBoundary = 100;
+
+const GameScreen = ({ userNumber, onGameOver }) => {
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  useEffect(() => {
+    if (currentGuess === userNumber) {
+      onGameOver();
+    }
+  }, [currentGuess, userNumber, onGameOver]);
+
+  const nextGuessHandler = (direction) => {
+    if (
+      (direction === "lower" && currentGuess < userNumber) ||
+      (direction === "greater" && currentGuess > userNumber)
+    ) {
+      Alert.alert(
+        "Don't lie",
+        "You know that this is not true, or have you forgotten that your number is: " +
+          userNumber +
+          " ?",
+        [{ text: "Sorry!", style: "cancel" }]
+      );
+      return;
+    }
+
+    if (direction === "lower") {
+      maxBoundary = currentGuess;
+    } else {
+      minBoundary = currentGuess + 1;
+    }
+
+    const newRndNumber = generateRandomBetween(
+      minBoundary,
+      maxBoundary,
+      currentGuess
+    );
+    setCurrentGuess(newRndNumber);
+  };
+
+  return (
+    <View style={styles.screen}>
+      <View>
+        <Title>Opponent's Guess</Title>
+        <NumberContainer>{currentGuess}</NumberContainer>
+      </View>
+
+      <View>
+        <Text>Higher or lower?</Text>
+        <View>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
+            -
+          </PrimaryButton>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
+            +
+          </PrimaryButton>
+        </View>
+        {/* + - */}
+      </View>
+
+      <View>{/* Log Rounds */}</View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: Colors.accent500,
+    textAlign: "center",
+    borderWidth: 2,
+    borderColor: Colors.accent500,
+    padding: 12,
+  },
+});
+
+export default GameScreen;
